01c773f49484c539bd60e8ef18aa372c6762a8b9
saifyali05 <istakbalhussainistakbalhussain@gmail.com>
2026-02-15 13:29:40 +0530

    Configure split deployment for Vercel and Render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api/polls',
+    baseURL: `${import.meta.env.VITE_API_URL || ''}/api/polls`,
 });
 
 export const createPoll = (data) => api.post('/', data);
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io();
+const socket = io(import.meta.env.VITE_API_URL || undefined);
 
 export default socket;
